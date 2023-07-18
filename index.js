2df4d467b0e0efc40c5c4d23c79aa7ab398cfa2e
Gabriel Laurentino <gabriellaurentino@hotmail.com>
2023-07-18 20:19:34 -0300

    asds

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ if (process.env.NODE_ENV !== "test") {
   app.use(
     "/doc",
     /*  authDocProd, */
-    swaggerUi.serve,
+    /* swaggerUi.serve, */
     swaggerUi.setup(swaggerFile, swaggerOptions)
   );
 }
