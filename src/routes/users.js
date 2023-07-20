7ea8d56dc2b1d71d194035a4d943b14e3138c89a
Gabriel Laurentino <gabriellaurentino@hotmail.com>
2023-07-20 17:09:04 -0300

    feat(users): login route conclude

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,13 +4,14 @@ var router = express.Router();
 const bcrypt = require("bcrypt");
 const userSchema = require("../models/user");
 const errorHandler = require("../functions/errorHandler");
+const jwt = require("jsonwebtoken");
 
 router.post("/create", connectDB, async (req, res, next) => {
   try {
     // #swagger.tags = ['User']
-    let { nome, email, password } = req.body;
+    let { nome, email, senha } = req.body;
     const hashTimesNumber = 10;
-    const hashPass = await bcrypt.hash(password, hashTimesNumber);
+    const hashPass = await bcrypt.hash(senha, hashTimesNumber);
     const DBres = await userSchema.create({
       nome,
       email,
@@ -22,6 +23,36 @@ router.post("/create", connectDB, async (req, res, next) => {
       statusMensagem: "Usuário cadastrado com sucesso!",
       resposta: DBres,
     });
+  } catch (error) {
+    return errorHandler(res, error);
+  }
+});
+
+router.post("/login", connectDB, async (req, res, next) => {
+  try {
+    // #swagger.tags = ['User']
+    let { email, senha } = req.body;
+
+    const DBres = await userSchema.findOne({ email }).select("+senha");
+    if (DBres) {
+      let correctPass = await bcrypt.compare(senha, DBres.senha);
+      if (correctPass) {
+        let token = jwt.sign({ id: DBres._id }, process.env.JWT_SECRET, {
+          expiresIn: "1d",
+        });
+
+        res.header("x-auth-token", token);
+        res.status(200).json({
+          status: "OK",
+          statusMensagem: "Usuário autenticado com sucesso!",
+          resposta: { "x-auth-token": token },
+        });
+      } else {
+        throw new Error("Email ou senha inválidos");
+      }
+    } else {
+      throw new Error("Email ou senha inválidos");
+    }
   } catch (error) {
     return errorHandler(res, error);
   }
